12fc2c0bb45096c5097e95d60da9444ce5f63613
Haras <testcase5428@gmail.com>
2019-03-07 15:09:13 +0100

    Added FightActivity

--- app/src/main/java/com/example/rharasimiuk/djangorestlearn/FightActivity.java
+++ app/src/main/java/com/example/rharasimiuk/djangorestlearn/FightActivity.java
@@ -3,51 +3,47 @@ package com.example.rharasimiuk.djangorestlearn;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 
-public class HuntActivity extends AppCompatActivity{
+public class FightActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hunt);
+        setContentView(R.layout.activity_fight);
+    }
+
+    public void escape(View view) {
 
     }
 
-    //Object to save variables
-    public static Bundle dataPacketHunt = new Bundle();
-
-    //Goblin's cave case
-    public void goblinsCave(View view) {
-
-        startActivity(new Intent(getApplicationContext(), FightActivity.class));
-        int expPlace = 0;
-        HuntActivity.dataPacketHunt.putInt("expPlace", expPlace);
-        finish();
+    public void spell(View view) {
 
     }
 
-    //Dragon's lair case
-    public void dragonsLair(View view) {
+    public void heal(View view) {
 
-        startActivity(new Intent(getApplicationContext(), FightActivity.class));
-        int expPlace = 1;
-        HuntActivity.dataPacketHunt.putInt("expPlace", expPlace);
-        finish();
+    }
+
+    public void explore(View view) {
+
+    }
+
+    public void startFight(View view) {
 
     }
 
     @Override
     public void onBackPressed() {
-
         AlertDialog.Builder exit = new AlertDialog.Builder(this);
         exit.setMessage("Are you sure you want to exit the application?")
                 .setNegativeButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        //saveSkills();
+                        //saveGame();
                         finish();
                         System.exit(0);
                     }
@@ -60,13 +56,8 @@ public class HuntActivity extends AppCompatActivity{
                 })
                 .create();
         exit.show();
-
     }
 
-    public void back(View view) {
 
-        startActivity(new Intent(getApplicationContext(), MainActivity.class));
-        finish();
 
-    }
 }
